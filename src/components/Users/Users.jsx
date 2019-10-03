2aa51a01f5f328de2e19c7c7e3baf5d2819df86c
Sergery Levchuk <s.levchuk@ashwood.by>
2019-10-03 16:24:55 +0300

    3.10.2019

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import Style from './Users.module.css'
-import * as axios from 'axios';
-import avatarDefault from '../../assets/img/logo.jpg'
+import Style from "./Users.module.css";
+import avatarDefault from "../../assets/img/logo.jpg";
 
 let Users = (props) => {
-    let getUsers = () => {
-        if (props.users.length === 0) {
-            axios.get(' https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                props.setUsers(response.data.items)
-            });
-        }
+
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
 
     return <div>
+        <div>
+            <ul className={Style.pagination}>
+                {pages.map(p => {
+                    return <li className={props.currentPage === p && Style.selected}
+                               onClick={() => {
+                                   props.onPageChanged(p);
+                               }}>{p}</li>
+                })}
+            </ul>
+        </div>
         {
             props.users.map(u =>
                 <div key={u.id} className={Style.user}>
--- src/redux/userReducer.js
+++ src/redux/userReducer.js
@@ -1,9 +1,18 @@
 const SET_USERS = 'SET_USERS';
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
+const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
 
 let initialState = {
-    users: []
+    users: [],
+    pageSize: 10,
+    totalUsersCount: 0,
+    currentPage: 1,
+    isFetching: true,
+
+    // users локальные
     // users: [
     //     {
     //         id: 'user_id_1',
@@ -53,18 +62,29 @@ const usersReducer = (state = initialState, action) => {
                 })
             };
         case SET_USERS: {
-            return {
-                ...state,
-                users: [...state.users, ...action.users]
-            }
+            return { ...state, users: action.users }
+        }
+        case SET_CURRENT_PAGE: {
+            return {...state, currentPage: action.currentPage}
+        }
+        case SET_TOTAL_USERS_COUNT: {
+            return {...state, totalUsersCount: action.count}
+        }
+        case TOGGLE_IS_FETCHING: {
+            return {...state, isFetching: action.isFetching}
         }
         default:
             return state;
     }
 };
 
-export const setUsersActionCreator = (users) => ({type: SET_USERS, users});
-export const followActionCreator = (userId) => ({type: FOLLOW, userId});
-export const unFollowActionCreator = (userId) => ({type: UNFOLLOW, userId});
+// Action Creator
+
+export const setUsers = (users) => ({type: SET_USERS, users});
+export const follow = (userId) => ({type: FOLLOW, userId});
+export const unFollow = (userId) => ({type: UNFOLLOW, userId});
+export const setCurrentPage = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage});
+export const setTotalUsersCount = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount});
+export const toggleIsFetching = (isFetching) => ({type: TOGGLE_IS_FETCHING, isFetching});
 
 export default usersReducer;
